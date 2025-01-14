e4bff9ce6e391870f8779ac6c5269f1dee732c91
Os <etu54028@henallux.be>
2025-01-14 15:37:00 +0100

    finis les equipements

--- funcpp/Main.cpp
+++ funcpp/Main.cpp
@@ -11,24 +11,5 @@
 
 int main(int argc, char* argv[]) {
 	//nettoyerLog();
-	Perso j{ "test",100,100,100,100,100 };
-	Casque oc{ "Casque d'os","Un casque fait d'os",25,25 };
-	Casque sc{ "non equipe","",0,0 };
-	Potion np{ "petite potion","Une potion d'un rouge vif qui restaure la sante",EffetsConsommable::soinPv,15 };
-	j.getInventaire()->ajouterConsommable(np);
-	j.getInventaire()->ajouterConsommable(np);
-	std::cout << j.toString();
-	j.changerEquipement(oc);
-	std::cout << j.toString();
-
-	j.getInventaire()->utiliserConsommable(0,j);
-	std::cout << j.toString();
-
-	j.changerEquipement(sc);
-	std::cout << j.toString();
-	j.getInventaire()->utiliserConsommable(0, j);
-
-
-
 	return EXIT_SUCCESS;
 }
